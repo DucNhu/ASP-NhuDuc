be226e62f7f2031d132c0f55102cac2dd400f835
DucNhu <60964371+DucNhu@users.noreply.github.com>
2020-11-01 15:48:38 +0700

    w05

---
+++ LAB01/WingtipToys/WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys") { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        //public DbSet<CartItem> ShoppinfCartItems { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderDetails> OrderDetailss { get; set; }
+    }
+}
--- LAB01/w02/w02/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="w02.Global" Language="C#" %>
