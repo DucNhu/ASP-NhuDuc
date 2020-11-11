eb2742b5edbc7a4c402c1a963a63e4e4eb687933
DucNhu <60964371+DucNhu@users.noreply.github.com>
2020-11-11 22:15:33 +0700

    ss8,9,10

--- ASP-Core/Newspaper1/Newspaper1/Startup.cs
+++ ASP-Core/Newspaper1/Newspaper1/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Newspaper1.Models;
 
 namespace Newspaper1
 {
@@ -33,6 +35,8 @@ namespace Newspaper1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = @"Server=DESKTOP-T7QA6K7\SQLEXPRESS;Database=Newspaper;Trusted_Connection=True;ConnecRetryCount=0";
+            services.AddDbContext<NewspaperContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
